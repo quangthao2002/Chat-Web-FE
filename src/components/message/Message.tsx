74280b260c3ba02db2ea29124f8b2172c4e54c0a
quangthao2002 <quangthao22122002@gmail.com>
2024-03-21 00:57:46 +0700

    fix assets remove file

--- src/components/message/Message.tsx
+++ src/components/message/Message.tsx
@@ -20,17 +20,11 @@ const Message = ({ message }) => {
             <img
               alt="Tailwind CSS chat bubble component"
               src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-
               // src={avatarClassName}
             />
           </div>
         </div>
-        {/* <div className="chat-header px-1">
-          Quang Thảo
-          <time className="text-xs opacity-50 ml-1">12:45</time>
-        </div> */}
         <div className={`chat-bubble text-white mt-1  ${bubbleBgColor}`}>{message.text}</div>
-        {/* <div className="chat-bubble chat-bubble-info">Hello</div> */}
         <div className="chat-footer opacity-50 text-xs gap-1 items-center">{formatTime}</div>
       </div>
     </>
