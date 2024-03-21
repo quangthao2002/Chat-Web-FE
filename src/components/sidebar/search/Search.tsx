50711748cbe0bd680749abcfc8f40bcd5826ed55
quangthao2002 <quangthao22122002@gmail.com>
2024-03-22 00:08:38 +0700

    handleSearch

--- src/components/sidebar/search/Search.tsx
+++ src/components/sidebar/search/Search.tsx
@@ -1,10 +1,28 @@
+import useGetConversations from "@/hooks/useGetConversations"
+import useConversation from "@/zustand/useConversation"
 import { useState } from "react"
+import toast from "react-hot-toast"
 import { IoSearchSharp } from "react-icons/io5"
 const Search = () => {
   const [search, setSearch] = useState("")
+  const { setSelectedConversation } = useConversation()
+  const { conversation } = useGetConversations()
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    if (search === "") return
+    if(search.length < 3) return toast.error("Search must be at least 3 characters")
+    const searchResult = conversation.find((conv) => conv.username.toLowerCase().includes(search.toLowerCase()))
+    if (searchResult) {
+      setSelectedConversation(searchResult)
+      setSearch("")
+    }else{
+      toast.error("No user found")
+    }
+  }
   return (
     <>
-      <form className="flex items-center gap-2 m-2">
+      <form className="flex items-center gap-2 m-2" onSubmit={handleSearch}>
         <input
           className="input input-bordered w-screen my-1 ml-1 bg-gray-200  rounded-full"
           type="text"
